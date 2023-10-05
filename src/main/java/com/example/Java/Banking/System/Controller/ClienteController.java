ae754b5f42803d40e14d02aadbeeed8510c48992
matias.barbosa <matias.abarbosa@gmail.com>
2023-10-05 18:00:03 -0300

    Controller cliente

--- src/main/java/com/example/Java/Banking/System/Controller/ClienteController.java
+++ src/main/java/com/example/Java/Banking/System/Controller/ClienteController.java
@@ -1,19 +1,16 @@
 package com.example.Java.Banking.System.Controller;
 
 
-import com.example.Java.Banking.System.Controller.Model.Cliente;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/cliente")
+@RequestMapping("clientes")
 public class ClienteController {
 
-    @GetMapping(value = "/cadastrar")
-    public String criaCliente(Cliente cliente) {
-        return "Hello cliente!";
+    @PostMapping
+    public void cadastraCliente(@RequestBody String json) {
+        System.out.println(json);
+
     }
 
 }
